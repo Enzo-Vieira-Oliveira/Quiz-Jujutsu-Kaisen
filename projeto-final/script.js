576e17807dcb18f0ef81268c6ba441becab2212d
Enzo-Vieira-Oliveira <fabrikadeetiquetas@hotmail.com>
2025-11-25 15:09:11 -0300

    Add files via upload

---
+++ projeto-final/script.js
@@ -0,0 +1,27 @@
+const perguntas = [
+    {
+        texto:'1) O que Sukuna, o principal vilão da obra, é?',
+        opcoes: ['uma maldição','um humano comum','um mestre de maldições','um feiticeiro jujutsu'],
+        correta: 3
+    },
+    {
+        texto:'2) Antes de Yuji se tornar o hospedeiro do Sukuna, a Escola Jujutsu já havia reunido quantos dedos do Rei das Maldições?',
+        opcoes: ['3 dedos','5 dedos','6 dedos','9 dedos'],
+        correta: 3
+    },
+    {
+        texto:'3) Black Flash é a técnica de Yuji Itadori. Que outro personagem, além dele, conseguiu usar esse ataque por mais vezes?',
+        opcoes: ['Aoi Todo','Nanami','Satoru Gojo','Megumi Fushiguro'],
+        correta: 2
+    },
+    {
+        texto:'4) Qual atriz faz o "tipo de mulher" de Yuji Itadori?',
+        opcoes: ['Margot Robbie','Emma Watson','Jennifer Lawrence','Scarlett Johansson'],
+        correta: 3
+    },
+    {
+        texto:'5) Qual a proporção da técnica amaldiçoada utilizada por Nanami?',
+        opcoes: ['7 por 3','7 por 7','7 por 8','7 por 10'],
+        correta: 1
+    },
+]
