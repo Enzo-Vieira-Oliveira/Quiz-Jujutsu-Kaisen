c4e0ef8f1840b98f10e2c06aebfe02facb0a3994
Enzo-Vieira-Oliveira <fabrikadeetiquetas@hotmail.com>
2025-12-02 15:26:06 -0300

    Update script.js

--- projeto-final/script.js
+++ projeto-final/script.js
@@ -24,4 +24,34 @@ const perguntas = [
         opcoes: ['7 por 3','7 por 7','7 por 8','7 por 10'],
         correta: 1
     },
+
 ]
+
+let indice = 0;
+let pontos = 0;
+
+function mostrarPergunta() {
+    const perguntaAtual = perguntas[indice];
+    document.getElementById('textoPergunta').textContent = perguntaAtual.texto;
+    document.getElementById('op0').textContent =  '' +perguntaAtual.opcoes[0];
+    document.getElementById('op1').textContent =  '' +perguntaAtual.opcoes[1];
+    document.getElementById('op2').textContent =  '' +perguntaAtual.opcoes[2];
+    document.getElementById('op3').textContent =  '' +perguntaAtual.opcoes[3];
+
+    document.getElementById('progresso').textContent = 'Pergunta ' + (indice + 1) + ' de ' + perguntas.length;
+
+    const radios = document.querySelectorAll('input[name="opcao"]');
+    radios.forEach(function (r) {
+        r.checked = false;
+    });
+
+    document.getElementById('aviso').textContent = '';
+}
+
+function iniciarQuiz() {
+    document.getElementById('telaInicial').classList.remove('tela-ativa');
+    document.getElementById('telaQuiz').classList.add('tela-ativa');
+    indice = 0;
+    pontos = 0;
+    mostrarPergunta();
+}
